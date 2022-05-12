37fd34857ebaff937f6594b87602e6de8df40f98
youngentry <90388461+youngentry@users.noreply.github.com>
2022-05-13 00:07:19 +0900

    5622 하다가 멈춤 이어서 할 것

---
+++ 5622.js
@@ -0,0 +1,14 @@
+const inputData = require("fs").readFileSync("devstdin").toString().split("");
+
+const timeDictinary = {
+  ABC: 2,
+  DEF: 3,
+  GHI: 4,
+  JKL: 5,
+  MNO: 6,
+  PQRS: 7,
+  TUV: 8,
+  WXYZ: 9,
+};
+console.log(timeDictinary(0));
+for (i = 0; i < inputData.length; i++) {}
