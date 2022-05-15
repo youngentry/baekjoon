19f67b6b7568d167ed73622171c5e778b4f78c1b
youngentry <90388461+youngentry@users.noreply.github.com>
2022-05-15 17:55:54 +0900

    2941 (문자열) 크로알파 - replace(), /word/gi
    https://www.acmicpc.net/problem/2941
    문제
    https://sakuraop.tistory.com/93
    풀이

--- 2941.js
+++ 2941.js
@@ -2,4 +2,4 @@ let inputData = require("fs").readFileSync("devstdin").toString().trim();
 
 let result = inputData.replace(/c=|c-|dz=|d-|lj|nj|s=|z=/g, "0");
 
-console.log(result.length);
+console.log(result, result.length);
