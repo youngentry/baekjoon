6679a6e67691edecb180583f33071cd1af10afa7
youngentry <90388461+youngentry@users.noreply.github.com>
2022-05-22 15:46:18 +0900

    10250 (기본수학1)ACM호텔 - 삼항연산자
    https://www.acmicpc.net/problem/10250
    문제
    https://sakuraop.tistory.com/94
    풀이

---
+++ 10250_ternary-operator.js
@@ -0,0 +1,24 @@
+const input = require("fs")
+  .readFileSync("devstdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((x) => x.trim());
+
+for (i = 1; i <= input[0]; i++) {
+  let [h, w, n] = input[i].split(" ");
+  for (j = 1; j <= h * w; j++) {
+    if (n == j) {
+      let y = "";
+      y = n % h == 0 ? (y = h) : (y = `${n % h}`);
+
+      let x = `${Math.ceil(j / h)}`;
+      if (x.length == 1) {
+        x = `0${x}`;
+      }
+
+      let yx = y + x;
+      console.log(yx);
+    }
+  }
+}
