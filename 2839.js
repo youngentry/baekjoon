80e5fa9b022d9d42b8dd54c24d8a673fa6bbe6a8
youngentry <90388461+youngentry@users.noreply.github.com>
2022-06-04 23:54:14 +0900

    2839 풀이중

--- 2839.js
+++ 2839.js
@@ -1,4 +1,14 @@
-let n = require("fs").readFileSync("devstdin").toString();
+let n = Number(require("fs").readFileSync("devstdin").toString());
 
-let n3 = 0;
-let n5 = 0;
+let kg3 = 0;
+
+while (true) {
+  if (n % 5 == 0) {
+    console.log(n / 5 + kg3);
+  }
+  if ((n % 5 == 1) | (n % 5 == 2)) {
+    console.log(-1);
+  }
+  kg3++;
+  n = +3;
+}
