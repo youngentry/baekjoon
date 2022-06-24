c02122815e60c7a6299b87d2d190371900b51468
youngentry <90388461+youngentry@users.noreply.github.com>
2022-06-25 00:52:57 +0900

    10870 피보나치수열 재귀함수
    https://sakuraop.tistory.com/143
    풀이

--- 10870.js
+++ 10870.js
@@ -1,10 +1,8 @@
 const n = require("fs").readFileSync("devstdin").toString();
 
 function fibo(n) {
-  if (n == 0) {
-    return 0;
-  } else if (n == 1) {
-    return 1;
+  if (n == 0 || n == 1) {
+    return n;
   } else {
     return fibo(n - 1) + fibo(n - 2);
   }
