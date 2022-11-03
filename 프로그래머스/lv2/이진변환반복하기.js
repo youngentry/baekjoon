242345cb429cb8c16250552a51b19a29c6304173
youngentry <90388461+youngentry@users.noreply.github.com>
2022-11-03 20:22:21 +0900

    Update 이진변환반복하기.js

--- 프로그래머스/lv2/이진변환반복하기.js
+++ 프로그래머스/lv2/이진변환반복하기.js
@@ -1,6 +1,6 @@
 function solution(s) {
-    const remove0 = (str) => {
-        return str.replaceAll("0", "");
+    const removeWord = (str, word) => {
+        return str.replaceAll(word, "");
     };
 
     const lengthTobinaryNumber = (str) => {
@@ -8,18 +8,17 @@ function solution(s) {
     };
 
     let removedS = s;
-    let removed0count = 0;
+    let removedWordCount = 0;
     let beforeLength;
     let tryCount = 0;
     while (removedS.split("").includes("0") || removedS.length !== 1) {
         tryCount++;
         beforeLength = removedS.length;
-        removedS = remove0(removedS);
-        removed0count += beforeLength - removedS.length;
+        removedS = removeWord(removedS, "0");
+        removedWordCount += beforeLength - removedS.length;
         removedS = lengthTobinaryNumber(removedS);
     }
-
-    return [tryCount, removed0count];
+    return [tryCount, removedWordCount];
 }
 
 solution("1111111111111111111111111111111");
