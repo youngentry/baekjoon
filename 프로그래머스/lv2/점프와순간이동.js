bbcd4c8b9c08e6e2aebddcdd12a7fb1563b6523c
youngentry <90388461+youngentry@users.noreply.github.com>
2022-11-26 21:26:10 +0900

    221126

--- 프로그래머스/lv2/점프와순간이동.js
+++ 프로그래머스/lv2/점프와순간이동.js
@@ -2,15 +2,14 @@ function solution(n) {
     let battery = 0;
 
     while (n > 0) {
-        // 순간이동: 현재까지 온 거리 x2
-        if (n % 2 === 0) {
-            n = n / 2;
-            continue; // 배터리 소모 없음
-        }
         // 점프: K칸을 점프하면 건전지가 사용됨
         if (n % 2 === 1) {
             n = n - 1;
             battery++; // 점프하면 배터리를 사용함
+        }
+        // 순간이동: 현재까지 온 거리 x2
+        if (n % 2 === 0) {
+            n = n / 2;
         }
     }
     return battery;
