a14bf3add2b056b0a9972bb2495277f0977e3608
youngentry <90388461+youngentry@users.noreply.github.com>
2022-11-26 15:22:34 +0900

    Update 영어끝말잇기.js

--- 프로그래머스/lv2/영어끝말잇기.js
+++ 프로그래머스/lv2/영어끝말잇기.js
@@ -4,36 +4,26 @@ function solution(n, words) {
 
     for (let i = 0; i < words.length; i++) {
         let number = (i % n) + 1;
+        const isInclude = saidWordArray.includes(words[i]) && i !== 0;
+        const isCoincide = words[i - 1] !== undefined && words[i][0] !== words[i - 1].at(-1);
+        const isRightWordLength = words[i].length === 1;
+        const isClear = i === words.length - 1;
 
-        // 포함되어 있으면 탈락
-        if (saidWordArray.includes(words[i]) && i !== 0) {
-            return [number, order];
-        }
         saidWordArray.push(words[i]);
-        // 글자가 일치하지 않으면 탈락
-        if (words[i - 1] !== undefined && words[i][0] !== words[i - 1].at(-1)) {
-            return [number, order];
-        }
-        // 길이가 1이면 탈락
-        if (words[i].length === 1) {
+
+        // 단어 포함/글자 불일치/길이 1 => 탈락
+        if (isInclude || isCoincide || isRightWordLength) {
             return [number, order];
         }
         // 끝말잇기에 문제 없으면 [0,0] 반환
-        if (i === words.length - 1) {
-            console.log([0, 0], 4);
+        if (isClear) {
             return [0, 0];
         }
-        // 몇 번째 말했는지 카운터
+        // 몇 번째로 말했는지 카운트
         if (number === n) {
             order++;
         }
     }
 }
-
-// n명이 번갈아 단어를 말한다
-// 한 글자 단어는 인정안된다 => 탈락
-// 이전에 등장한 단어면 안된다 => 탈락
-// 앞사람이 말한 단어의 마지막 문자로 시작하는 단어를 말한다 => 아니면 탈락
-// 아무도 탈락하지 않으면 [0,0]을 반환한다.
 
 solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]);
