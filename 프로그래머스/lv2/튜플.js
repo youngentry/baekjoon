f8b0b676507eda1d038c17037157bcd1de8d6297
youngentry <90388461+youngentry@users.noreply.github.com>
2022-11-28 01:11:37 +0900

    Update 튜플.js

--- 프로그래머스/lv2/튜플.js
+++ 프로그래머스/lv2/튜플.js
@@ -1,9 +1,14 @@
 function solution(s) {
     const sliceSideBracketsS = s.slice(2, -2);
+    console.log(sliceSideBracketsS);
     const tupleArray = sliceSideBracketsS.split("},{");
+    console.log(tupleArray);
     const sortedTupleArray = tupleArray.sort((a, b) => a.length - b.length);
+    console.log(sortedTupleArray);
     const joinedTupleArray = sortedTupleArray.join(",");
+    console.log(joinedTupleArray);
     const sortedTupleArrayToSet = new Set(joinedTupleArray.split(","));
+    console.log(sortedTupleArrayToSet);
     const tuple = [...sortedTupleArrayToSet].map((el) => parseInt(el));
     return tuple;
 }
